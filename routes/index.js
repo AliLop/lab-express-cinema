4b620e98ded165b4aa969a790cddd167567c9f54
AliLop”git config --global user.email alicia.hl87@gmail.comgit config --global user.name AliLop” <65405578+AliLop@users.noreply.github.com>
2020-11-09 14:46:57 +0000

    done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('../models/Movie');
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+/* GET /movies page */
+router.get('/movies', (req, res) => {
+  Movie.find()
+  .then((allTheMoviesFromDB) => {
+    console.log(allTheMoviesFromDB)
+    res.render('movies', { movies : allTheMoviesFromDB})
+  });
+});
+
+router.get('/movies/:movieId', (req, res) => {
+    let movieId = req.params.movieId;
+    console.log(movieId);
+
+    Movie.findById(movieId)
+    .then((theOneMovie) => {
+        res.render('movie-details', {movie: theOneMovie})
+    })
+    .catch((err) => {
+      console.log( `Error is ${err}`)
+      res.render('not-found',  {err});
+    })
+});
 
 module.exports = router;
